454c81f8634a7d04430d2aa757582ce4fee55dc6
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-10-11 20:14:53 +0300

    updated styles

---
+++ static/js/base.js
@@ -0,0 +1,27 @@
+const arrow = document.getElementById("arrow");
+const profilePopupSettings = document.getElementById("profile-popup-settings");
+const alertCloseButton = document.getElementById("alert__close");
+const alertBlock = document.getElementById("alert");
+
+let isPopupVisible = false;
+
+if (arrow != null) {
+    arrow.addEventListener("click", () => {
+        arrow.classList.toggle("rotate");
+
+        if (isPopupVisible) {
+            profilePopupSettings.style.display = "none";
+        } else {
+            profilePopupSettings.style.display = "flex";
+        }
+
+        isPopupVisible = !isPopupVisible
+
+    });
+}
+
+if (alertCloseButton != null) {
+    alertCloseButton.addEventListener("click", () => {
+        alertBlock.style.display = "none";
+    });
+}
