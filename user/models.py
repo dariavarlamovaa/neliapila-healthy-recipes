30b552c4bfbc9fc3f8dc116f36273fbbca9d7e6d
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-10-13 21:10:21 +0300

    changed str method

--- user/models.py
+++ user/models.py
@@ -13,4 +13,4 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.first_name, self.last_name
+        return self.username
