d37eb5caf883de3139f5b9ca19287f4eb7048ab3
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-10-16 15:24:15 +0300

    changed first_name and last_name to name

--- user/models.py
+++ user/models.py
@@ -4,8 +4,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200, blank=True, null=True)
-    last_name = models.CharField(max_length=200, blank=True, null=True)
+    name = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(max_length=500, blank=True, null=True, unique=True)
     username = models.CharField(max_length=50, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
