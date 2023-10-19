7190b8f4b5cf8d47bdc3a073d85235fb06cc98d5
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-10-19 14:56:11 +0300

    added recipe form

---
+++ recipes/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.forms import ModelForm
+
+from .models import Recipe
+
+
+class NewRecipe(ModelForm):
+    class Meta:
+        model = Recipe
+        exclude = ['author', 'is_approved', 'date_created']
+        labels = {'image': 'Recipe image'}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['image'].widget = forms.FileInput(attrs={'accept': 'image/*'})
+        self.fields['ingredients'].widget = forms.Textarea(
+            attrs={'placeholder': 'Ingredient1 - 100g\nIngredient2 - 2 spoons'})
+        self.fields['steps'].widget = forms.Textarea(
+            attrs={'placeholder': 'Step 1 - ...\nStep 2 - ...'})
