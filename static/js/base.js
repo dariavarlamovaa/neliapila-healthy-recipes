5adbd37a9acdee49834e791a6fca9a592dc99fe4
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-10-19 14:57:44 +0300

    added script for search-bar

--- static/js/base.js
+++ static/js/base.js
@@ -2,8 +2,11 @@ const arrow = document.getElementById("arrow");
 const profilePopupSettings = document.getElementById("profile-popup-settings");
 const alertCloseButton = document.getElementById("alert__close");
 const alertBlock = document.getElementById("alert");
+const searchLogo = document.getElementById("search_logo")
+const searchBar = document.getElementById("search-bar")
 
 let isPopupVisible = false;
+let isSearchBarVisible = false
 
 if (arrow != null) {
     arrow.addEventListener("click", () => {
@@ -15,8 +18,19 @@ if (arrow != null) {
             profilePopupSettings.style.display = "flex";
         }
 
-        isPopupVisible = !isPopupVisible
+        isPopupVisible = !isPopupVisible;
     });
+}
+
+if (searchLogo != null) {
+    searchLogo.addEventListener("click", () => {
+        if (isSearchBarVisible) {
+            searchBar.style.display = "none";
+        } else {
+            searchBar.style.display = "block"
+        }
+        isSearchBarVisible = !isSearchBarVisible;
+    })
 }
 
 if (alertCloseButton != null) {
