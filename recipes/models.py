1f6afe2c645dc1e0ee5a1841c6fb5cf725c0f96d
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-10-19 14:58:48 +0300

    changed model`s fields

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,14 @@
+import datetime
+import os
+from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
+
+
+def recipe_image_path(instance, filename):
+    author_name = instance.author.username
+    post_date = timezone.now().date()
+    return os.path.join('recipes_images', str(post_date), author_name, filename)
 
 
 class Recipe(models.Model):
@@ -15,14 +24,17 @@ class Recipe(models.Model):
         (True, 'Approved'),
         (False, 'On moderation'),
     ]
-    author = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=255)
     portions = models.PositiveIntegerField(default=1, blank=True, null=True)
     cooking_hours = models.PositiveIntegerField(default=0)
     cooking_minutes = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='recipe_images/%(title)s/')
+    image = models.ImageField(upload_to=recipe_image_path)
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    ingredients = models.JSONField(default=dict)
+    ingredients = models.TextField()
     steps = models.TextField()
     is_approved = models.BooleanField(default=False, choices=STATUS_CHOICES)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
