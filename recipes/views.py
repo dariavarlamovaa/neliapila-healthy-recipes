e4a6d80009f1252e92aa393d423524e96d3897d0
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-10-19 14:59:32 +0300

    added validation check

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+from recipes.forms import NewRecipe
+
+
+def add_a_recipe(request):
+    profile = request.user
+    form = NewRecipe()
+    if request.method == 'POST':
+        form = NewRecipe(request.POST, request.FILES)
+        if form.is_valid():
+            user_recipe = form.save(commit=False)
+            user_recipe.author = profile
+            user_recipe.save()
+            messages.success(request,
+                             'The recipe has been added successfully. Wait for the admin\'s approval')
+            return redirect('profile', request.user.id)
+
+    content = {'form': form}
+    return render(request, 'recipes/add_recipe.html', content)
