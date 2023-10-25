3e58720e58658109be61a3c805397bb286641d5b
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-10-25 20:17:34 +0300

    added capitalize() to title

--- recipes/views.py
+++ recipes/views.py
@@ -12,10 +12,10 @@ def add_a_recipe(request):
         if form.is_valid():
             user_recipe = form.save(commit=False)
             user_recipe.author = profile
+            user_recipe.title = user_recipe.title.capitalize().strip()
             user_recipe.save()
             messages.success(request,
                              'The recipe has been added successfully. Wait for the admin\'s approval')
             return redirect('profile', request.user.id)
-
     content = {'form': form}
     return render(request, 'recipes/add_recipe.html', content)
