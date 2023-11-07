5236df22e959bd963c09d9fe798771cea970fb0e
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-11-07 21:52:37 +0300

    added get_pdf url

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from recipes import views
 
 urlpatterns = [
     path('add-a-recipe/', views.add_a_recipe, name='add-recipe'),
-    path('recipe/<pk>', views.show_specific_recipe, name='specific-recipe')
+    path('recipe/<pk>', views.show_specific_recipe, name='specific-recipe'),
+    path('pdf-view/<pk>', views.get_pdf, name='pdf')
 ]
