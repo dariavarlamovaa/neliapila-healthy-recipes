607c7df03398af5cfa8e5a6d7f484a53e98302e3
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-11-11 20:34:39 +0300

    added found recipes url

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.get_recipes, name='recipes'),
+    path('found-recipes/', views.search_recipes, name='found-recipes'),
     path('', include('user.urls'))
 ]
