cb27aa8cda11c1b1342bb80ef19fcfab7e63a3dc
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-11-16 20:21:23 +0300

    added delete recipe and comment urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,10 @@ from recipes import views
 urlpatterns = [
     path('add-a-recipe/', views.add_a_recipe, name='add-recipe'),
     path('recipe/<pk>', views.show_specific_recipe, name='specific-recipe'),
-    path('pdf-view/<pk>', views.get_pdf, name='pdf')
+
+    path('pdf-view/<pk>', views.get_pdf, name='pdf'),
+
+    path('delete-recipe/<pk>', views.delete_recipe, name='delete-recipe'),
+
+    path('delete-comment/<pk>', views.delete_comment, name='delete-comment')
 ]
