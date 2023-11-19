f9f95d3d9e4dca21eb9487801807a85bdee14816
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-11-19 19:28:42 +0300

    added authors and about us url

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.get_recipes, name='recipes'),
     path('found-recipes/', views.search_recipes, name='found-recipes'),
-    path('', include('user.urls'))
+    path('', include('user.urls')),
+
+    path('authors/', views.authors, name='authors'),
+
+    path('about-us/', views.about_us, name='about-us')
 ]
