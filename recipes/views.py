ea9bdb774b44b992fd1d9f5310884e466c78cea1
Daria Varlamova <Daria.novozhilova2000@mail.ru>
2023-11-21 20:39:43 +0300

    added confirmations of the comments and recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 import re
 
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from xhtml2pdf import pisa
 from django.db import IntegrityError
@@ -54,12 +55,13 @@ def leave_a_comment(request, specific_recipe):
 
 def delete_comment(request, pk):
     comment = Comment.objects.get(pk=pk)
-    if not request.user.id == comment.owner.id:
+    if request.user.id == comment.owner.id or request.user.is_staff:
+        if request.method == 'GET':
+            comment.delete()
+            messages.success(request, 'You have deleted your comment')
+            return redirect('specific-recipe', pk=comment.recipe.id)
+    else:
         return HttpResponseForbidden('You do not have permission to access this page.')
-    if request.method == 'GET':
-        comment.delete()
-        messages.success(request, 'You have deleted your comment')
-        return redirect('specific-recipe', pk=comment.recipe.id)
 
 
 def show_specific_recipe(request, pk):
@@ -117,13 +119,59 @@ def get_pdf(request, pk):
 def delete_recipe(request, pk):
     recipe_to_delete = Recipe.objects.get(pk=pk)
 
-    if not request.user.id == recipe_to_delete.author.id:
+    if request.user.id == recipe_to_delete.author.id or request.user.is_staff:
+        if request.method == 'POST':
+            recipe_to_delete.delete()
+            messages.success(request, f'Recipe - "{recipe_to_delete.title}" successfully deleted')
+            return redirect('profile', request.user.id)
+    else:
         return HttpResponseForbidden('You do not have permission to access this page.')
-
-    if request.method == 'POST':
-        recipe_to_delete.delete()
-        messages.success(request, f'Recipe - "{recipe_to_delete.title}" successfully deleted')
-        return redirect('profile', request.user.id)
 
     context = {'recipe': recipe_to_delete}
     return render(request, 'recipes/delete.html', context)
+
+
+@staff_member_required
+def pending_recipes(request):
+    all_pending_recipes = Recipe.objects.filter(is_approved=False).all()
+    context = {'recipes': all_pending_recipes}
+    return render(request, 'recipes/pending-recipes.html', context)
+
+
+@staff_member_required
+def approve_recipe(request, pk):
+    recipe = Recipe.objects.get(pk=pk)
+    form = NewRecipe(instance=recipe)
+    if 'image' in form.fields:
+        del form.fields['image']
+    if request.method == 'GET':
+        return render(request, 'recipes/one-pending-recipe.html',
+                      {'recipe': recipe, 'form': form})
+    else:
+        try:
+            form = NewRecipe(request.POST, instance=recipe)
+            recipe.is_approved = True
+            form.save()
+            return redirect('pending-recipes')
+        except ValueError:
+            messages.error(request, 'Something went wrong')
+            return render(request, 'recipes/one-pending-recipe.html',
+                          {'form': form})
+
+
+@staff_member_required
+def pending_comments(request):
+    all_pending_comments = Comment.objects.filter(is_approved=False).all()
+    if request.method == 'GET':
+        context = {'comments': all_pending_comments}
+        return render(request, 'recipes/pending-comments.html', context)
+
+
+@staff_member_required
+def approve_comment(request, pk):
+    comment = Comment.objects.get(pk=pk)
+    print(comment)
+    if request.method == 'GET':
+        comment.is_approved = True
+        comment.save()
+        return redirect('pending-comments')
